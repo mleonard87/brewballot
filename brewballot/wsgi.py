70d424d4452e2240b6de5f69e4c9998a6b9a9bac
Mike Leonard <mike.r.leonard@gmail.com>
2014-11-26 21:43:36 +1100

    Configuration changes to allow the app to run on heroku and locally.

--- brewballot/wsgi.py
+++ brewballot/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brewballot.settings.production")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+import os
 
-application = Cling(get_wsgi_application())
+application = None
+if os.environ.get('DEPLOY_ENVIRONMENT') == 'heroku':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brewballot.settings.production")
+    from dj_static import Cling
+    application = Cling(get_wsgi_application())
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brewballot.settings.local")
+    application = get_wsgi_application()
+
+
